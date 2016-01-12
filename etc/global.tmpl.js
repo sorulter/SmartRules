c41b344bfc6d95c2b578cf58ba392a466ac2c918
Frank Kung <kfanjian@gmail.com>
2016-01-13 01:07:23 +0800

    Send redirect if is local domain, .cn domain or domain DNS resolve is private ip.

--- etc/global.tmpl.js
+++ etc/global.tmpl.js
@@ -4,9 +4,19 @@ var httpProxy = 'PROXY {{ .server }}; DIRECT';
 function FindProxyForURL(url, host) {
     if (url.substring(0, 4) == "ftp:")
         return direct;
-    if (host.indexOf(".local", host.length - 6) !== -1) {
+    if (shExpMatch(url, "*.local"))
         return direct;
-    }
+
+    if (shExpMatch(host, "*.cn")) {
+        return "DIRECT";
+    };
+
+    // Private domain, send direct.
+    if (isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+        isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
+        isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0") ||
+        isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0"))
+        return direct;
 
     return httpProxy;
 }
