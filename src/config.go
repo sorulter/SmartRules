dc468520d35cf39e33b85abfb6f6c42f4a9b0ff2
Frank Kung <kfanjian@gmail.com>
2016-01-12 22:52:07 +0800

    Add init config method.

---
+++ src/config.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	c "github.com/gocubes/config"
+)
+
+var (
+	config struct {
+		HttpHostPort string   `json:"http_host_port"`
+		DefaultList  []string `json:"default_list"`
+		DSN          string
+		Hashids      struct {
+			Salt   string `json:"salt"`
+			MinLen int    `json:"min_len"`
+		}
+	}
+)
+
+func initConfig() {
+	provider, _ := c.New("etc/config.json", "json")
+	provider.Get(&config)
+}
